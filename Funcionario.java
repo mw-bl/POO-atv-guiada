2c7f216423a4695f2cffbddaa43f6c4ce2defbc0
mw-bl <bezerram533@gmail.com>
2023-12-07 16:46:33 -0300

    :sparkles: feat: Getter e Setters do Funcionario

--- Funcionario.java
+++ Funcionario.java
@@ -10,4 +10,28 @@ public class Funcionario {
         this.salario = salario;
         this.cargo = cargo;
     }
+    // Getter/Setter NOME
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    // Getter/Setter IDADE
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    // Getter/Setter SALARIO
+    public double getSalario() {
+        return salario;
+    }
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    // Getter/Setter CARGO
 }
