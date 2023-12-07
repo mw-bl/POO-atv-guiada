72800811d6cb133f75ad5e4705509dbd71c7a3bf
mw-bl <bezerram533@gmail.com>
2023-12-07 16:33:06 -0300

    :sparkles: feat: Construtor Funcionario

--- Funcionario.java
+++ Funcionario.java
@@ -3,4 +3,11 @@ public class Funcionario {
     int idade;
     double salario;
     String cargo;
+
+    public Funcionario(String nome, int idade, double salario, String cargo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.salario = salario;
+        this.cargo = cargo;
+    }
 }
