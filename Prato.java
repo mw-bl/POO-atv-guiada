52d11a59ecc6d6613d197b9eafa5521ceb3aaf04
mw-bl <bezerram533@gmail.com>
2023-12-07 16:39:08 -0300

    :sparkles: feat: Construtor Prato

--- Prato.java
+++ Prato.java
@@ -2,4 +2,10 @@ public class Prato {
     String nomePrato;
     String descricao;
     double preco;
+
+    public Prato(String nomePrato, String descricao, double preco) {
+        this.nomePrato = nomePrato;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
 }
